52a488deca44c1c8ace3257f56f23e05ec53cd3c
Frédéric Lassinot <fredlass@gmail.com>
2026-02-02 21:23:38 +0100

    ajout component add

--- src/app/components/add-task/add-task.ts
+++ src/app/components/add-task/add-task.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Task } from './../../model/Task';
+import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
-  templateUrl: './add-task.html',
-  styleUrl: './add-task.scss',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './add-task.html'
 })
-export class AddTask {
+export class AddTask implements OnInit {
+  form!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private taskService: TaskService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.nonNullable.group({
+      title: ['', Validators.required],
+      description: [''],
+      status: <Task['status']>'todo'
+    });
+  }
+
+  save(): void {
+    if (this.form.invalid) return;
+
+    const task: Omit<Task, 'id'> = this.form.getRawValue(); 
+    this.taskService.addTask(task).subscribe(() => {
+      this.router.navigate(['/board']);
+    });
+  }
 }
